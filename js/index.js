23e6e9907fa0ec49af52aac80ffb0c17dd9cdf26
victorawogbemi <vawogbemi@gmail.com>
2020-01-29 18:16:52 -0500

    presentation commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+var data = require('./Murder-on-the-2nd-Floor-Raw-Data.json'); //with path
+var valuesOnly = Object.values(data);
+
 
 function openNav() {
   document.getElementById("mySidebar").style.width = "500px";
@@ -9,19 +12,14 @@ function closeNav() {
   document.getElementById("main").style.marginLeft= "0";
 }
 
-function openNav2(index) {
-  
-  
-    if(index == 1){
-        document.getElementById("data").innerHTML = "woiwionasfoansfoa";
-    }
-     if(index == 3){
-        document.getElementById("data").innerHTML = "woiwionasfoansfoa";
-    }
-    else{
-        document.getElementById("data").innerHTML = "you passed";
+function openNav2(t) {
+var text = t;
+    
+  document.getElementById("text").innerHTML = text;
+  document.getElementById("data").innerHTML = valuesOnly;
 
-    }
+    
+   
   document.getElementById("mySidebar2").style.width = "500px";
   document.getElementById("main2").style.marginRight = "500px";
       
@@ -31,3 +29,5 @@ function closeNav2() {
   document.getElementById("mySidebar2").style.width = "0";
   document.getElementById("main2").style.marginRight= "0";
 }
+
+console.log(valuesOnly);
